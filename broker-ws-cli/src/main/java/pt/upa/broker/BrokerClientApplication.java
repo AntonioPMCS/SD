647954ad910b980c4aea1b55c22c49bab1e4057e
AntonioPMCS <apmcarmosilva@ist.utl.pt>
2016-03-29 10:00:37 +0100

    First implementation of broker service. Just returning test strings.

--- broker-ws-cli/src/main/java/pt/upa/broker/BrokerClientApplication.java
+++ broker-ws-cli/src/main/java/pt/upa/broker/BrokerClientApplication.java
@@ -1,10 +1,18 @@
 package pt.upa.broker;
 
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+
+// classes generated from WSDL
+import pt.upa.broker.ws.
+
+
 public class BrokerClientApplication {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println(BrokerClientApplication.class.getSimpleName() + " starting...");
 
+		
+		
 	}
 
 }
--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -1,7 +1,40 @@
 package pt.upa.broker.ws;
 
+import javax.jws.WebService;
+
+import java.util.List;
+
+@WebService(
+    endpointInterface="pt.upa.broker.ws.BrokerPortType",
+    wsdlLocation="broker.1_0.wsdl",
+    name="BrokerWebService",
+    portName="BrokerPort",
+    targetNamespace="http://ws.broker.upa.pt/",
+    serviceName="BrokerService"
+)
 public class BrokerPort {
 
-	// TODO
+	public String ping (String name) {
+		return "Method ping() was called with name parameter: "+name;
+	}
 
-}
+	public String requestTransport (String origin, String destination, int price)
+		throws InvalidPriceFault_Exception, UnavailableTransportFault_Exception, UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception {
+
+		return "Method requestTransport() was called with origin: "+origin+" destination: "+destination+"and price: "+price;
+	}
+
+	public TransportView viewTransport (String id) throws UnknownTransportFault_Exception {
+		TransportView transportView = new TransportView();
+		transportView.setId("Method viewTransport() was called with id: "+id);
+		return transportView;
+	}
+
+	public List<TransportView> listTransports() {
+		ListTransportsResponse listTransports = new ListTransportsResponse();
+		return listTransports.getReturn();
+	}
+
+	public void clearTransports() {} // TODO
+
+}	
