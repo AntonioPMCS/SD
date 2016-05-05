ce8a93bc3c738af0915bfd176218a56fcc7737a2
AntonioPMCS <goncalocg@gmail.com>
2016-05-05 17:49:03 +0100

    More on broker handler

--- upa-library/cryptography-set/src/main/java/pt/upa/crypt/Cypher.java
+++ upa-library/cryptography-set/src/main/java/pt/upa/crypt/Cypher.java
@@ -1,5 +1,21 @@
 package pt.upa.crypt;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 public class Cypher {
 
+	public byte[] cypherWithPrivateKey(byte[] content, PrivateKey key) throws IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException{
+		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		byte[] cipheredContent = cipher.doFinal(content);
+		return cipheredContent;
+	}
+	
 }
--- upa-library/handlers-set/src/main/java/pt/upa/handlers/BrokerHandler.java
+++ upa-library/handlers-set/src/main/java/pt/upa/handlers/BrokerHandler.java
@@ -1,6 +1,10 @@
 package pt.upa.handlers;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -13,14 +17,19 @@ import javax.xml.ws.handler.soap.SOAPMessageContext;
 import org.w3c.dom.Element;
 
 import pt.upa.ca.ws.cli.AuthorityClient;
+import pt.upa.crypt.Cypher;
 import pt.upa.crypt.Digest;
 import pt.upa.crypt.SecureRandomGen;
-
+import static javax.xml.bind.DatatypeConverter.printHexBinary;
 
 @SuppressWarnings("restriction")
 public class BrokerHandler implements SOAPHandler<SOAPMessageContext> {
 	ByteArrayOutputStream baos = new ByteArrayOutputStream();
 	AuthorityClient CA = null;
+	private final String BROKER_STORE_PASS = "ins3cur3";
+	private final String BROKER_KEY_PASS = "1nsecure";
+	private final String BROKER_CERTIFICATE_ALIAS = "UpaBroker";
+	private final String CA_CERTIFICATE_ALIAS = "UpaBroker";
 	
 	public Set<QName> getHeaders() {
         return null;
@@ -56,7 +65,32 @@ public class BrokerHandler implements SOAPHandler<SOAPMessageContext> {
     private void handleOutgoingMsg(SOAPMessageContext smc) {
     	SOAPMessage message = smc.getMessage();
     	try {
-    		
+    		//Get Keystore
+    		String keystoreFilename = "./UpaBrokerSecurity/UpaBroker.jks";
+    	    FileInputStream fIn = new FileInputStream(keystoreFilename);
+    	    KeyStore keystore = KeyStore.getInstance("JKS");
+    	    keystore.load(fIn, BROKER_STORE_PASS.toCharArray());
+    	    		/*
+    	    		 * tens em keystore a key store UpaBroker.jks onde tens:
+    	    		 * - Certificado da CA
+    	    		 * - Seu próprio Certificado
+    	    		 * - a sua chave privada
+    	    		 */
+    	    
+    	    //Gets Broker Certificate
+    	    String brokerCertName = BROKER_CERTIFICATE_ALIAS;
+    	    Certificate cert = keystore.getCertificate(brokerCertName);
+    	    if(cert == null){
+    	    	System.out.println("Certificate "+brokerCertName+" doesn't exist.");
+    	    }
+    	    
+    	    //Gets CA Certificate
+    	    String CACertName = CA_CERTIFICATE_ALIAS;
+    	    Certificate cert2 = keystore.getCertificate(CACertName);
+    	    if(cert2 == null){
+    	    	System.out.println("Certificate "+CACertName+" doesn't exist.");
+    	    }
+    	    
         	//Convert Soap msg passed to string
 			message.writeTo(baos);
 			String convertedSoap = baos.toString();
@@ -67,14 +101,20 @@ public class BrokerHandler implements SOAPHandler<SOAPMessageContext> {
 	        
 			//Digest Message
 			Digest digestor = new Digest();
-			digestor.digestMessage(convertedSoap);
+			byte[] digestedMsg = digestor.digestMessage(convertedSoap);
 			
-			//Put two together
+			//Get Private Key from Broker Certificate
+			PrivateKey pk = (PrivateKey) keystore.getKey("UpaBroker", BROKER_KEY_PASS.toCharArray());
 			
+			Cypher cypher = new Cypher();
 			
-			//Cipher with own privateKey
+			byte[] cipheredRandom = cypher.cypherWithPrivateKey(random, pk);
+			byte[] cipheredDigestMsg = cypher.cypherWithPrivateKey(digestedMsg, pk);
 			
-			
+			System.out.println(printHexBinary(random));
+			System.out.println(printHexBinary(cipheredRandom));
+			System.out.println(printHexBinary(digestedMsg));
+			System.out.println(printHexBinary(cipheredDigestMsg));
 			//Put result on soad header
 			
 		} catch (Exception e1) {
@@ -109,3 +149,20 @@ public class BrokerHandler implements SOAPHandler<SOAPMessageContext> {
     }
 
 }
+/*
+ * // import helper methods to print byte[]
+import static javax.xml.bind.DatatypeConverter.parseBase64Binary;
+import static javax.xml.bind.DatatypeConverter.printBase64Binary;
+
+...
+
+    // encoding binary data with base 64
+    System.out.println("Encoding to Base64 ...");
+    String result = printBase64Binary(cipherBytes);
+
+...
+
+    // decoding string in base 64
+    System.out.println("Decoding from Base64 ...");
+    byte[] result = parseBase64Binary(cipherText);
+*/
